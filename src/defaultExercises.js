cd55c6e05965d02c50db4ff9a9bbf7d41778d835
Lemonish <patrikmihelson@gmail.com>
2024-09-28 14:36:11 +0300

    added muscle group to defaultexercises

--- src/defaultExercises.js
+++ src/defaultExercises.js
@@ -1,3 +1,14 @@
-const defaultExercises = ["Bench Press", "Pull Up", "Bent Over Row", "Deadlift", "Hip Hinge", "Leg Press", "Squat", "Incline Dumbell Press", "Lateral Raise", "Calf Raise"]
+const defaultExercises = [
+  { name: "Bench Press", muscleGroup: "Chest" },
+  { name: "Pull Up", muscleGroup: "Back" },
+  { name: "Bent Over Row", muscleGroup: "Back" },
+  { name: "Deadlift", muscleGroup: "Back" },
+  { name: "Hip Hinge", muscleGroup: "Hamstrings" },
+  { name: "Leg Press", muscleGroup: "Legs" },
+  { name: "Squat", muscleGroup: "Legs" },
+  { name: "Incline Dumbell Press", muscleGroup: "Chest" },
+  { name: "Lateral Raise", muscleGroup: "Shoulders" },
+  { name: "Calf Raise", muscleGroup: "Calves" }
+];
 
-export default defaultExercises
+export default defaultExercises;
