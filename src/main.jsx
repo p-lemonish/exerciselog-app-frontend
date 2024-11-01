1f79053eafc2825ac6c2d9f57dce7a15cb9a2475
Lemonish <patrikmihelson@gmail.com>
2024-11-01 17:19:55 +0200

    added CustomThemeProvider to give appropriate background and component colors based on dark or light mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import { createRoot } from "react-dom/client"
 import App from "./App"
-import './index.css'
-import { AuthProvider } from './context/AuthContext'
+import "./index.css"
+import { AuthProvider } from "./context/AuthContext"
+import CustomThemeProvider from "./theme/CustomThemeProvider"
 
-const container = document.getElementById('root')
+const container = document.getElementById("root")
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App/>
+      <CustomThemeProvider>
+        <App/>
+      </CustomThemeProvider>
     </AuthProvider>
   </React.StrictMode>
 )
---
+++ src/theme/CustomThemeProvider.jsx
@@ -0,0 +1,18 @@
+import React, { useMemo } from "react";
+import ThemeProvider from "@mui/material";
+import { lightTheme, darkTheme } from "./theme"
+import { useMediaQuery } from "@mui/material";
+
+// eslint-disable-next-line react/prop-types
+function CustomThemeProvider({ children }) {
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+
+    const theme = useMemo(() => (prefersDarkMode ? darkTheme : lightTheme), [prefersDarkMode])
+
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+export default CustomThemeProvider
