20a448a28bbf1952f0e5bd5c8eda2be728260bfa
Lemonish <patrikmihelson@gmail.com>
2024-11-12 16:53:51 +0200

    added PWA support (works locally at the moment)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     outDir: 'dist',
-  }
-})
+  },
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http:localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'ExerciseLog App',
+        short_name: 'ExerciseLogApp',
+        description: '',
+        theme_color: '#1976d2',
+        icons: [
+          {
+            src: 'pwa-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
